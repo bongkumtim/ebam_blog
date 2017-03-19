c9c7bfe3ea09da6bcf9e41ac7784728159ca0ac3
Bong <mjbond_bong2@yahoo.com>
2017-03-19 17:07:09 +0800

    configure comment/create nested routes/ tested created and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :find, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@posts = Post.all.order(created_at: :desc)
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 		if @post.save
 		   redirect_to @post
 		else
@@ -22,11 +22,18 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
 	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to root_path
 	end
@@ -49,4 +56,9 @@ class PostsController < ApplicationController
 	def post_params
 		params.require(:post).permit(:title,:content)
 	end
+
+	def find
+		@post = Post.find(params[:id])
+	end
+
 end
